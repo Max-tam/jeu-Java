2fdaad06818c180ac2eb6ec5cb8797b8955b493d
Max-tam <maxime.tamarin@etu.univ-amu.fr>
2023-02-14 14:08:23 +0100

    ajout de commentaire sur la Class Artefact

--- Artefact.java
+++ Artefact.java
@@ -2,16 +2,16 @@ public class Artefact extends Effet {
 
     private String nomArtefact;
 
-    public Artefact(String effet, String nomArtefact) {
+    public Artefact(String effet, String nomArtefact) { // constructeur de la Class Artefact
         super(effet);
         this.nomArtefact = nomArtefact;
     }
 
-    public String getNomArtefact() {
+    public String getNomArtefact() { // recupere le nom de l'artefact
         return nomArtefact;
     }
 
-    public void setNomArtefact(String nomArtefact) {
+    public void setNomArtefact(String nomArtefact) { // defini le nom de l'artefact
         this.nomArtefact = nomArtefact;
     }
 
