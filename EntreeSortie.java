e431459dee30bf1b0e9672f068f7e55aa56e34c5
Max-tam <maxime.tamarin@etu.univ-amu.fr>
2023-02-14 16:44:21 +0100

    ajout fichier Main.java pour executer des testes

--- EntreeSortie.java
+++ EntreeSortie.java
@@ -5,9 +5,9 @@ public class EntreeSortie {
     public void debutPartie() // affichage du titre
     {
         System.out.println();
-        System.out.println("======================================");
-        System.out.println("======| Bienvenue sur Java RPG |======");
-        System.out.println("======================================");
+        System.out.println("|====================================|");
+        System.out.println("|=====| Bienvenue sur Java RPG |=====|");
+        System.out.println("|====================================|");
         System.out.println();
     }
 
@@ -16,22 +16,29 @@ public class EntreeSortie {
         System.out.println();
         System.out.println("======| Menu Principal |======");
         System.out.println();
-        System.out.println("------|   JOUER : 1   |------");
-        System.out.println("------| PARAMETRE : 2 |------");
-        System.out.println("------|   CREDIT : 3  |------");
+        System.out.println("-----------------------------");
+        System.out.println("|-----|   JOUER : 1   |-----|");
+        System.out.println("|-----| PARAMETRE : 2 |-----|");
+        System.out.println("|-----|   CREDIT : 3  |-----|");
+        System.out.println("-----------------------------");
         System.out.println();
 
         Scanner sc1 = new Scanner(System.in);
+        System.out.print("-> ");
         int choixMenuPrincipal = sc1.nextInt();
+        System.out.println();
         sc1.close();
         switch (choixMenuPrincipal) {
             case 1:
+                System.out.println("**Lance le jeu**");
                 // Lancer le jeu
                 break;
             case 2:
+                System.out.println("**Parametre**");
                 // Lancer Menu Parametre
                 break;
             case 3:
+                System.out.println("**Credit**");
                 // Lancer Credit
                 break;
             default:
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        EntreeSortie titreJeux = new EntreeSortie();
+        titreJeux.debutPartie();
+        titreJeux.menuPrincipal();
+    }
+    
+}
